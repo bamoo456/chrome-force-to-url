f974ad1af6c379b5f30e7e06ab3f7a1071752a97
GeorgeChen <bamoo456@gmail.com>
2016-12-06 17:44:55 +0800

    add periodly update on target url

--- popup.js
+++ popup.js
@@ -11,37 +11,76 @@ Reference:http://www.cnblogs.com/guogangj/p/3235703.html
 var IS_SCRIPT_STARTED = false;
 
 
-document.getElementById('startScript').onclick = function() {
+document.getElementById('startScript').onclick = function () {
     if (!IS_SCRIPT_STARTED) {
         IS_SCRIPT_STARTED = true;
-        console.log('---> start');
+        startScripts();
     }
 }
 
-document.getElementById('stopScript').onclick = function() {
+document.getElementById('stopScript').onclick = function () {
     if (IS_SCRIPT_STARTED) {
         IS_SCRIPT_STARTED = false;
-        console.log('---> stop')
     }
 }
+
+var targetUrl = 'https://tw.yahoo.com/';
+var count = 0;
+
+function startScripts() {
+    periodlyUpdate();
+}
+
+function periodlyUpdate() {
+    setTimeout(function () {
+        chrome.tabs.query({}, function (tabs) {
+            tabs.forEach(function (tab) {
+                if (tab.url === targetUrl) {
+                    console.log('---> try to update the url')
+                    chrome.tabs.sendRequest(tab.id, {
+                        action: "getDom"
+                    }, function (response) {
+                        console.log(response);
+                        console.log('----ready to update to yahoo');
+                        chrome.tabs.update(tab.id, {
+                            url: 'http://tw.yahoo.com'
+                        });
+                        // ready to next run
+                        if (IS_SCRIPT_STARTED) periodlyUpdate();
+                    });
+                }
+            });
+        });
+    }, 1000);
+}
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.tabs.getSelected(null, function (tab) {
-        // Send a request to the content script.
-        setTimeout(function(){
-        chrome.tabs.sendRequest(tab.id, {
-            action: "getDom"
-        }, function (response) {
-            console.log(response);
-        });
-        }, 3000)
-    });
-    // chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    // var url = tabs[0].url;
-    // $("#msgLabel").text(url);
-    // chrome.tabs.create({ url: url }, function(tab) {
-    //     chrome.tabs.remove(tab.id);
+    for (var i=0; i< 2; i++) {
+        chrome.tabs.create({url: 'https://tw.yahoo.com', active: false});
+    }
+    // chrome.tabs.getSelected(null, function (tab) {
+    //     // Send a request to the content script.
+    //     setTimeout(function () {
+    //         chrome.tabs.sendRequest(tab.id, {
+    //             action: "getDom"
+    //         }, function (response) {
+    //             console.log(response);
+    //         });
+    //     }, 3000)
     // });
+    // chrome.tabs.query({
+    //     'active': true,
+    //     'lastFocusedWindow': true
+    // }, function (tabs) {
+    //     var url = tabs[0].url;
+    //     $("#msgLabel").text(url);
+    //     chrome.tabs.create({
+    //         url: url
+    //     }, function (tab) {
+    //         chrome.tabs.remove(tab.id);
+    //     });
     // });
 });
