03d15ddf21c39525c574a17a8b5fbe7ec01c3cb2
GeorgeChen <bamoo456@gmail.com>
2016-12-06 16:58:04 +0800

    add popup buttons

--- popup.js
+++ popup.js
@@ -6,6 +6,26 @@ Date:2016/7/25
 Reference:http://www.cnblogs.com/guogangj/p/3235703.html
           https://developer.chrome.com/extensions
 */
+
+
+var IS_SCRIPT_STARTED = false;
+
+
+document.getElementById('startScript').onclick = function() {
+    if (!IS_SCRIPT_STARTED) {
+        IS_SCRIPT_STARTED = true;
+        console.log('---> start');
+    }
+}
+
+document.getElementById('stopScript').onclick = function() {
+    if (IS_SCRIPT_STARTED) {
+        IS_SCRIPT_STARTED = false;
+        console.log('---> stop')
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.getSelected(null, function (tab) {
         // Send a request to the content script.
